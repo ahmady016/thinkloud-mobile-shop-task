29bb0311769a5df65ed131ec2fe91e01c8e67629
ahmady016 <ahmady09@gmail.com>
2021-04-08 14:06:14 +0200

    Add app constants

---
+++ src/_helpers/constants.js
@@ -0,0 +1,24 @@
+export const MOBILES = 'mobiles'
+export const BRANDS = [
+  'Sony',
+  'Samsung',
+  'Apple',
+  'Nokia',
+  'LG',
+]
+export const MEMORIES = [
+  '16GB',
+  '32GB',
+  '64GB',
+  '128GB',
+]
+export const COLORS = [
+  'white',
+  'black',
+  'gold',
+]
+export const SCREEN_SIZES = [
+  '4',
+  '5',
+  '6',
+]
--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
-import LS from './_helpers/localStorage'
-import mobilesData from './_helpers/mobiles.json'
-
 import App from './App'
 import './index.css'
 
-export const MOBILES = 'mobiles'
-
 // seed localStorage with data
+import LS from './_helpers/localStorage'
+import { MOBILES } from './_helpers/constants'
+import mobilesData from './_helpers/mobiles.json'
 const mobiles = LS.get(MOBILES)
 if (!mobiles) LS.set(MOBILES, mobilesData)
 
