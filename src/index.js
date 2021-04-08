725ea54b729d4e5f95063bf94a82f65e4da3e600
ahmady016 <ahmady09@gmail.com>
2021-04-08 13:04:06 +0200

    Add some helpers, and seed data in localStorage

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
+import LS from './_helpers/localStorage'
+import mobilesData from './_helpers/mobiles.json'
+
 import App from './App'
 import './index.css'
+
+export const MOBILES = 'mobiles'
+
+// seed localStorage with data
+const mobiles = LS.get(MOBILES)
+if (!mobiles) LS.set(MOBILES, mobilesData)
 
 render(
 	<React.StrictMode>
