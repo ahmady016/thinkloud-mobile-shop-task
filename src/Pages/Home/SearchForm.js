f110bd866efaf0169c869c4579ac5d2692a3e617
ahmady016 <ahmady09@gmail.com>
2021-04-08 22:22:07 +0200

    Add SearchForm header

--- src/Pages/Home/SearchForm.js
+++ src/Pages/Home/SearchForm.js
@@ -10,6 +10,7 @@ import { searchMobiles } from '../../_redux_store/mobiles'
 import { BRANDS } from '../../_helpers/constants'
 
 import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 import FormControl from '@material-ui/core/FormControl'
 import InputLabel from '@material-ui/core/InputLabel'
 import TextField from '@material-ui/core/TextField'
@@ -19,6 +20,11 @@ import Button from '@material-ui/core/Button'
 import SearchIcon from '@material-ui/icons/Search'
 import AddIcon from '@material-ui/icons/Add'
 import Alert from '@material-ui/lab/Alert'
+
+import styled from 'styled-components'
+const SearchFormContainer = styled.div`
+	margin: 1rem 0;
+`
 
 const schema = yup.object().shape({
 	model: yup.string().required(),
@@ -48,66 +54,69 @@ function SearchForm() {
 	}
 
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<Grid container spacing={3}>
-				<Grid item md={9} xs={12}>
-					<Controller
-						name="model"
-						control={control}
-						render={({ field }) => (
-							<FormControl fullWidth>
-								<TextField {...field} label="Model" />
-							</FormControl>
+		<SearchFormContainer>
+			<Typography variant="h5">Search Mobiles</Typography>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Grid container spacing={3}>
+					<Grid item md={9} xs={12}>
+						<Controller
+							name="model"
+							control={control}
+							render={({ field }) => (
+								<FormControl fullWidth>
+									<TextField {...field} label="Model" />
+								</FormControl>
+							)}
+						/>
+						{errors.model && (
+							<Alert severity="error">{errors.model.message}</Alert>
 						)}
-					/>
-					{errors.model && (
-						<Alert severity="error">{errors.model.message}</Alert>
-					)}
-					<Controller
-						name="brand"
-						control={control}
-						render={({ field }) => (
-							<FormControl fullWidth>
-								<InputLabel>Brand</InputLabel>
-								<Select {...field}>
-									{BRANDS.map((brand) => (
-										<MenuItem key={brand} value={brand}>
-											{brand}
-										</MenuItem>
-									))}
-								</Select>
-							</FormControl>
+						<Controller
+							name="brand"
+							control={control}
+							render={({ field }) => (
+								<FormControl fullWidth>
+									<InputLabel>Brand</InputLabel>
+									<Select {...field}>
+										{BRANDS.map((brand) => (
+											<MenuItem key={brand} value={brand}>
+												{brand}
+											</MenuItem>
+										))}
+									</Select>
+								</FormControl>
+							)}
+						/>
+						{errors.brand && (
+							<Alert severity="error">{errors.brand.message}</Alert>
 						)}
-					/>
-					{errors.brand && (
-						<Alert severity="error">{errors.brand.message}</Alert>
-					)}
+					</Grid>
+					<Grid item md={3} xs={12}>
+						<div className="w-100 h-100 mt-05 flex-column-around">
+							<Button
+								type="submit"
+								variant="outlined"
+								color="secondary"
+								fullWidth
+								startIcon={<AddIcon />}
+								onClick={goToAddMobile}
+							>
+								Add New Mobile
+							</Button>
+							<Button
+								type="submit"
+								variant="outlined"
+								color="primary"
+								fullWidth
+								startIcon={<SearchIcon />}
+							>
+								Search
+							</Button>
+						</div>
+					</Grid>
 				</Grid>
-				<Grid item md={3} xs={12}>
-					<div className="w-100 h-100 mt-05 flex-column-around">
-						<Button
-							type="submit"
-							variant="outlined"
-							color="secondary"
-							fullWidth
-							startIcon={<AddIcon />}
-							onClick={goToAddMobile}
-						>
-							Add New Mobile
-						</Button>
-						<Button
-							type="submit"
-							variant="outlined"
-							color="primary"
-							fullWidth
-							startIcon={<SearchIcon />}
-						>
-							Search
-						</Button>
-					</div>
-				</Grid>
-			</Grid>
-		</form>
+			</form>
+		</SearchFormContainer>
 	)
 }
 
